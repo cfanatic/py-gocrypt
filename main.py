68975b36fe89367a262c07b14d69278a988149a0
Arnd <git@codefanatic.de>
2019-03-12 22:40:22 +0100

    Refactor code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Call gocryptfs with the required arguments taken from a configuration file
+Call gocryptfs from terminal with command line arguments
 """
 
 import getopt
@@ -9,7 +9,43 @@ import subprocess
 import sys
 import toml
 
+# Set the path to the TOML configuration file
 CONFIG_PATH = "/Users/cfanatic/Coding/Python/GoCrypt/misc/config.toml"
+
+
+def decrypt(config, key):
+    # Call gocryptfs process
+    path_cipher = config[key]["cipher"]
+    path_plain = config[key]["plain"]
+    subprocess.run(["gocryptfs", path_cipher, path_plain])
+
+def encrypt(config, key):
+    # Encrypt folder by calling unmount process
+    path_plain = config[key]["plain"]
+    subprocess.run(["umount", path_plain])
+
+def mount():
+    # Get all folders which are currently mounted
+    return subprocess.check_output(["mount"])
+
+def auto(config, key):
+    # Automatically encrypt or decrypt folder
+    path = config[key]["plain"].encode()
+    if path in mount():
+        encrypt(config, key)
+    else:
+        decrypt(config, key)
+
+def summary(config):
+    # Print summary of all folders
+    index = 0
+    print("{0:<8} {1:<14} {2:<10} {3:<26}".format("Index", "Key", "Mount", "Path"))
+    print("----------------------------------------------------------------")
+    for key, value in config.items():
+        index += 1
+        path = config[key]["plain"]
+        active = "on" if path.encode() in mount() else "off"
+        print("{0:<8} {1:<14} {2:<10} {3:<26}".format(index, key, active, value["plain"]))
 
 def main():
     config = toml.load(CONFIG_PATH)
@@ -17,37 +53,20 @@ def main():
         if len(sys.argv) == 1:
             print("Error@main: Not enough input arguments given!")
         else:
-            opts, dummy = getopt.getopt(sys.argv[1:], "e:d:p", ["encrypt", "decrypt", "print"])
-            for opt, arg in opts:
+            opts, args = getopt.getopt(sys.argv[1:], "d:e:s", ["decrypt", "encrypt", "summary"])
+            for opt, key in opts:
                 if opt in ("-e", "--encrypt"):
-                    print("Info@main: Encrypting folder.")
-                    subprocess.run(["umount", config[arg]["plain"]])
+                    encrypt(config, key)
                 elif opt in ("-d", "--decrypt"):
-                    print("Info@main: Decrypting folder.")
-                    subprocess.run(["gocryptfs", config[arg]["cipher"], config[arg]["plain"]])
-                elif opt in ("-p", "--print"):
-                    index = 0
-                    print("{0:<8} {1:<14} {2:<10} {3:<26}".format("Index", "Key", "Mount", "Path"))
-                    print("----------------------------------------------------------------")
-                    for key, value in config.items():
-                        index += 1
-                        mount = subprocess.check_output(["mount"])
-                        path = config[key]["plain"].encode()
-                        active = "on" if path in mount else "off"
-                        print("{0:<8} {1:<14} {2:<10} {3:<26}".format(index, key, active, value["plain"]))
-                else:
-                    mount = subprocess.check_output(["mount"])
-                    path = config[sys.argv[1]]["plain"].encode()
-                    arg = sys.argv[1]
-                    if path in mount:
-                        print("Info@main: Encrypting folder.")
-                        subprocess.run(["umount", config[arg]["plain"]])
-                    else:
-                        print("Info@main: Decrypting folder.")
-                        subprocess.run(["gocryptfs", config[arg]["cipher"], config[arg]["plain"]])
-
+                    decrypt(config, key)
+                elif opt in ("-s", "--summary"):
+                    summary(config)
+            for key in args:
+                auto(config, key)
     except KeyError as e:
         print("Error@main: '" + e.args[0] + "' is not an encrypted folder!")
+    except getopt.GetoptError as e:
+        print("Error@main: " + e.args[0] +"!")
 
 if __name__ == "__main__":
     main()
